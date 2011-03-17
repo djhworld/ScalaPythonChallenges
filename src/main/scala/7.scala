46a98d838b785750ce8c7e01df5f8eeaf289b35c
Daniel Harper <djharperuk@gmail.com>
2011-03-17 23:18:41 +0000

    redone with SBT

---
+++ src/main/scala/7.scala
@@ -0,0 +1,34 @@
+import javax.imageio.{ImageIO}
+import java.io.{File}
+
+object ChallengeSeven extends Application {
+    val image = ImageIO.read(new File("images/oxygen.png"))
+    val subImage = image.getSubimage(0,43,608,9)
+    val colorSamplePoints = scala.List.range(0, subImage.getWidth, 7)
+    val result = colorSamplePoints.map { pointX =>
+                                            decodeFromColor( subImage.getRGB(pointX, 0) ).getOrElse(' ')
+                                       }.mkString
+
+    println("Result of analysis: " + result)
+    val bracketsRegEx =  "(\\[.*?\\])".r
+    val nextLevelStrArray = bracketsRegEx.findFirstIn(result).get.replaceAll("(\\[)|(\\])","").split(",")
+    val answer = nextLevelStrArray.map { item => item.trim.toInt.toChar }
+    println("Answer = " + answer.mkString)
+
+    def isGreyScale(c: java.awt.Color) :Boolean = {
+        if(c.getRed == c.getGreen && c.getRed == c.getBlue && c.getGreen == c.getBlue) {
+            return true
+        }
+        false
+    }
+
+    def decodeFromColor(color :Int) :Option[Char] = {
+        val c = new java.awt.Color(color)
+            if(isGreyScale(c)) {
+            return Some(c.getRed.toChar)
+        }
+        return None
+    }
+}
+
+// vim: set ts=4 sw=4 et:
